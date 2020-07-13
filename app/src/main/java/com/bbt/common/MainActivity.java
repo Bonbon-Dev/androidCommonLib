cc11069cc123cb796f90f74296e37165485745f0
lixiaonan <472379153@qq.com>
2020-07-13 11:48:40 +0800

    测试网络api代码

--- app/src/main/java/com/bbt/common/MainActivity.java
+++ app/src/main/java/com/bbt/common/MainActivity.java
@@ -2,13 +2,11 @@ package com.bbt.common;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
+import android.telephony.TelephonyManager;
 import android.widget.TextView;
 
-import com.bbt.commonlib.operationutil.ProcessUtils;
-import com.bbt.commonlib.operationutil.Utils;
-import com.bbt.commonlib.toolutil.StrToNumberUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tvOne = findViewById(R.id.tvOne);
+        TextView tvTwo = findViewById(R.id.tvTwo);
+        TextView tv3 = findViewById(R.id.tv3);
+        TextView tv4 = findViewById(R.id.tv4);
+
+
+        TelephonyManager telMag = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+
+        String simOperator = telMag.getSimOperator();
+        tvOne.setText("getSimOperator===="+simOperator);
+        tvTwo.setText("getNetworkOperator==="+telMag.getNetworkOperator());
+        tv3.setText("getSimOperatorName===="+telMag.getSimOperatorName()+"");
+        tv4.setText("getNetworkOperatorName===="+telMag.getNetworkOperatorName());
     }
 }
